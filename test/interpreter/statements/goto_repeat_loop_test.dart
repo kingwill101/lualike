d1ce290db8d1ae337007669e3782564d9d072d01
Glenford Williams <gw_dev@outlook.com>
2025-07-01 16:46:18 -0500

    Add goto repeat loop test (#14)

---
+++ test/interpreter/statements/goto_repeat_loop_test.dart
@@ -0,0 +1,35 @@
+import 'package:lualike/testing.dart';
+
+void main() {
+  group('Goto with repeated loops', () {
+    test('goto repeats a for loop until condition met', () async {
+      var program = <AstNode>[
+        LocalDeclaration([Identifier('i')], [], [NumberLiteral(0)]),
+        Label(Identifier('doagain')),
+        ForLoop(
+          Identifier('j'),
+          NumberLiteral(1),
+          NumberLiteral(2),
+          NumberLiteral(1),
+          [
+            Assignment(
+              [Identifier('i')],
+              [BinaryExpression(Identifier('i'), '+', NumberLiteral(1))],
+            ),
+          ],
+        ),
+        IfStatement(
+          BinaryExpression(Identifier('i'), '<', NumberLiteral(5)),
+          [],
+          [Goto(Identifier('doagain'))],
+          [],
+        ),
+        ExpressionStatement(Identifier('i')),
+      ];
+
+      var vm = Interpreter();
+      await vm.run(program);
+      expect(vm.globals.get('i'), equals(Value(6)));
+    });
+  });
+}
