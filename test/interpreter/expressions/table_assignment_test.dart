29bc1cdc426655a5219ad175309970694e39ca75
Glenford Williams <gw_dev@outlook.com>
2025-07-01 14:31:18 -0500

    test: table assignment uses identifier name when undefined

---
+++ test/interpreter/expressions/table_assignment_test.dart
@@ -0,0 +1,31 @@
+@Tags(['expressions'])
+import 'package:lualike/testing.dart';
+
+void main() {
+  group('Table assignments with undefined identifier keys', () {
+    test('dot syntax uses field name when identifier is undefined', () async {
+      final bridge = LuaLike();
+      await bridge.runCode('''
+        t = {}
+        t.undef = 42
+      ''');
+      final t = bridge.getGlobal('t') as Value;
+      final map = t.raw as Map<dynamic, dynamic>;
+      expect(map['undef'], equals(Value(42)));
+    });
+
+    test(
+      'bracket syntax uses identifier name when variable is undefined',
+      () async {
+        final bridge = LuaLike();
+        await bridge.runCode('''
+        t = {}
+        t[undef] = 99
+      ''');
+        final t = bridge.getGlobal('t') as Value;
+        final map = t.raw as Map<dynamic, dynamic>;
+        expect(map['undef'], equals(Value(99)));
+      },
+    );
+  });
+}
