cc9f6e09b8b218b4b9b1f8ee6976b563b8b733ab
Glenford Williams <gw_dev@outlook.com>
2025-07-01 16:46:18 -0500

    Add goto repeat loop test (#14)

---
+++ test/interpreter/statements/goto_loop_exit_test.dart
@@ -0,0 +1,37 @@
+import 'package:lualike/testing.dart';
+
+void main() {
+  group('Goto with loops', () {
+    test('goto exits a for loop to outer label', () async {
+      var program = <AstNode>[
+        LocalDeclaration([Identifier('i')], [], [NumberLiteral(0)]),
+        Label(Identifier('again')),
+        ForLoop(
+          Identifier('j'),
+          NumberLiteral(1),
+          NumberLiteral(10),
+          NumberLiteral(1),
+          [
+            Assignment(
+              [Identifier('i')],
+              [BinaryExpression(Identifier('i'), '+', NumberLiteral(1))],
+            ),
+            IfStatement(
+              BinaryExpression(Identifier('i'), '>=', NumberLiteral(5)),
+              [],
+              [Goto(Identifier('done'))],
+              [],
+            ),
+          ],
+        ),
+        Goto(Identifier('again')),
+        Label(Identifier('done')),
+        ExpressionStatement(Identifier('i')),
+      ];
+
+      var vm = Interpreter();
+      await vm.run(program);
+      expect(vm.globals.get('i'), equals(Value(5)));
+    });
+  });
+}
