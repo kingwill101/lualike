44fb63d82dcb1c768cfd89738a2f4231c8424829
Glenford Williams <gw_dev@outlook.com>
2025-07-20 15:51:45 -0500

    Extend timeout for bitwise script test

--- test/scripts/bitwise_script_test.dart
+++ test/scripts/bitwise_script_test.dart
@@ -3,13 +3,17 @@ import 'dart:io';
 import 'package:test/test.dart';
 
 void main() {
-  test('luascripts/test/bitwise.lua runs to completion', () async {
-    final result = await Process.run('dart', [
-      'run',
-      'bin/main.dart',
-      'luascripts/test/bitwise.lua',
-    ]);
-    expect(result.exitCode, equals(0));
-    expect(result.stdout.toString().trim().endsWith('OK'), isTrue);
-  });
+  test(
+    'luascripts/test/bitwise.lua runs to completion',
+    () async {
+      final result = await Process.run('dart', [
+        'run',
+        'bin/main.dart',
+        'luascripts/test/bitwise.lua',
+      ]);
+      expect(result.exitCode, equals(0));
+      expect(result.stdout.toString().trim().endsWith('OK'), isTrue);
+    },
+    timeout: const Timeout(Duration(seconds: 60)),
+  );
 }
