b5c3e6360b1b847d22e88011154a5bd421df1a09
kingwill101 <hey@glenfordwilliams.com>
2026-07-08 10:47:09 -0500

    feat(exports): expose new doc model types in public API
    Export AccessScope, AliasDescriptor, AliasDoc, AliasVariant,
    ConstantDescriptor, DocDescriptor, DocParam, EnumDescriptor, EnumDoc,
    FieldDoc, FunctionDescriptor, FunctionDoc, GenericParam, OperatorDoc,
    OverloadDoc, TableDescriptor, TableDoc, ValueDoc

--- pkgs/lualike/lib/docs.dart
+++ pkgs/lualike/lib/docs.dart
@@ -2,4 +2,23 @@ library;
 
 export 'src/docs/metadata_format.dart';
 export 'src/docs/renderer.dart';
-export 'src/stdlib/doc.dart' show FieldDoc, TableDoc;
+export 'src/stdlib/doc.dart'
+    show
+        AccessScope,
+        AliasDescriptor,
+        AliasDoc,
+        AliasVariant,
+        ConstantDescriptor,
+        DocDescriptor,
+        DocParam,
+        EnumDescriptor,
+        EnumDoc,
+        FieldDoc,
+        FunctionDescriptor,
+        FunctionDoc,
+        GenericParam,
+        OperatorDoc,
+        OverloadDoc,
+        TableDescriptor,
+        TableDoc,
+        ValueDoc;
